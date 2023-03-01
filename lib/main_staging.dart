9e7bc820681a3593bbdf866ca4d7336b737889fe
Nazareno Cavazzon <cavazzonnazareno@gmail.com>
2023-03-01 19:58:01 -0300

    Init project

---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:courtavenue/app/app.dart';
+import 'package:courtavenue/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
---
+++ test/app/view/app_test.dart
@@ -0,0 +1,12 @@
+import 'package:courtavenue/app/app.dart';
+import 'package:courtavenue/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+}
